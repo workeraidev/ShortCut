f60ce3000eaf677c235b1d768ef597a6d87a3f4b
Makzo <workerai.dev@gmail.com>
2025-10-06 10:11:40 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'as

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
